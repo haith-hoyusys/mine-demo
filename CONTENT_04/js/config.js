574bf89856272b4c4d649cff898ad05d457fde47
Hoang Hai <hai.th@gif.vn>
2026-09-04 15:53:23 +0700

    clone c04

---
+++ CONTENT_04/js/config.js
@@ -0,0 +1,9 @@
+var CONFIG = {
+  speed_ms: 16, // 1m/s=16px/s
+  data: [
+    [5, 3, 8, 3, 5, 6], // question 1
+    [6, 6, 8, 5, 4, 2, true, "center-center", 2, -1], // question 2
+    [6, 3, 2, 5, 7, 5, true, "center-left", 2, -1], // question 3
+    [6, 3, 3, 3, 6, 7, true, "center-left", 2, -1] // question 4
+  ]
+};
